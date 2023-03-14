8c86750d23382e1bf931865a85e086bd3875fc35
vx007 <vasilyev.x@mail.ru>
2023-03-14 23:29:01 +0300

    Commit #2

--- src/main/java/EmployeeDAO.java
+++ src/main/java/EmployeeDAO.java
@@ -2,9 +2,12 @@ import java.util.List;
 
 public interface EmployeeDAO {
     void create(Employee employee);
+
     Employee readById(int id);
+
     List<Employee> readAll();
-    void updateAgeById(int id, int age);
-    void updateCityById(int id, City city);
-    void deleteById(int id);
+
+    void update(Employee employee);
+
+    void delete(Employee employee);
 }
